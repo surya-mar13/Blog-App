644eb8902b09d1772a19804fec54a8efbf147dc9
Surya13 <suryaabhiramg@gmail.com>
2026-05-26 10:54:43 +0530

    update

--- Blog-Frontend/react/src/apiConfig.js
+++ Blog-Frontend/react/src/apiConfig.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://skillforge-backend-2.onrender.com";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://backend-2-jc5u.onrender.com";
